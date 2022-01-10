8a7dab131a4d94d512b7bb6516cea4c029dd48bf
Nasirul Islam <nasirul.islam@northsouth.edu>
2022-01-11 03:10:09 +0600

    init

---
+++ README.md
@@ -0,0 +1,12 @@
+# Terraform Crash Course 
+
+- Download and install terraform 
+- Setup terraform 
+- Download the cloud provider plugin ( provider )
+
+
+## Write the terraform scripts 
+
+- Follow the terraform website for more info
+- First add the providers
+- 
---
+++ main.tf
@@ -0,0 +1,52 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-southeast-1"
+  shared_credentials_file = "/home/maya/.aws/credentials"
+  profile = "softopark"
+}
+
+resource "aws_instance" "terraform" {
+  ami           = "ami-055d15d9cfddf7bd3"
+  key_name        = "dockertest"
+  instance_type = "t2.micro"
+  tags = {
+      Name = "built-with-terraform"
+  }
+  user_data = <<-EOF
+              #!/bin/bash
+              echo 'echo "##########################"'   >>  /home/ubuntu/.bashrc
+              echo 'echo "#                        #"'   >>  /home/ubuntu/.bashrc
+              echo 'echo "#  Built with Terraform  #"'   >>  /home/ubuntu/.bashrc
+              echo 'echo "#                        #"'   >>  /home/ubuntu/.bashrc
+              echo 'echo "#                        #"'   >>  /home/ubuntu/.bashrc
+              echo 'echo "##########################"'   >>  /home/ubuntu/.bashrc
+              sudo apt update -y && sudo apt upgrade -y
+              #######################"Installing Docker & compose"###########################
+              curl -fsSL https://get.docker.com -o get-docker.sh
+              sh get-docker.sh
+              sudo usermod -aG docker ubuntu
+              sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+              sudo chmod +x /usr/local/bin/docker-compose
+              #######################"Installing certbot"####################################
+              yes '' | sudo add-apt-repository ppa:certbot/certbot
+              sudo apt install python3-certbot-nginx -y
+              #######################"Installing nginx and breaking it"############
+              sudo apt install nginx -y
+              sudo chown ubuntu:ubuntu /usr/share/nginx/html/ -R
+              sudo chown ubuntu:ubuntu /var/www/html -R
+              sudo bash -c 'echo "Hello!" > /var/www/html/index.html'
+             EOF
+}
+
+output "IP" {
+  value = aws_instance.terraform.public_ip
+}
